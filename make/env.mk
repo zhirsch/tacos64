8ef7597983e865b268845f0991ef64bc4e0af3ca
Zachary Hirsch <zhirsch@umich.edu>
2018-12-31 15:11:40 -0500

    Use system gcc and clean up flags and linker script.

--- make/env.mk
+++ make/env.mk
@@ -1,5 +1,5 @@
 AS = $(CC)
-CC = x86_64-elf-gcc
+CC = gcc
 LD = $(CC)
 
 ASFLAGS = -DASM_FILE
@@ -8,24 +8,26 @@ CFLAGS = \
 	-Wall \
 	-Werror \
 	-Wextra \
+	-std=c11 \
+	-m64 \
 	-ffreestanding \
-	-fno-asynchronous-unwind-tables \
-	-mcmodel=large \
-	-mno-mmx \
-	-mno-red-zone \
-	-mno-sse \
-	-mno-sse2 \
-	-std=c11
+	-fno-pie \
+	-ffunction-sections \
+	-fdata-sections \
+	-mcmodel=kernel \
+	-mno-red-zone
 
 CPPFLAGS = -Iinclude
 
 LDFLAGS = \
+	-static \
+	-ffreestanding \
+	-nostdlib \
+	-Wl,--build-id=none \
 	-Wl,--fatal-warnings \
 	-Wl,--gc-sections \
 	-Wl,--print-gc-sections \
 	-Wl,-z,defs \
-	-Wl,-z,max-page-size=4096 \
-	-ffreestanding \
-	-nostdlib
+	-Wl,-z,max-page-size=4096
 
 LDLIBS = -lgcc
