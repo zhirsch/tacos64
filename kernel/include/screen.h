bfdb8178efcc2abbf13a88b1eae80d94ae31385c
Zachary Hirsch <zhirsch@umich.edu>
2024-02-07 02:25:14 -0500

    Add screen_printf.

--- kernel/include/screen.h
+++ kernel/include/screen.h
@@ -2,5 +2,6 @@
 #define SCREEN_H
 
 extern void screen_clear(void);
+extern void screen_printf(const char* fmt, ...);
 
 #endif /* SCREEN_H */
--- kernel/src/screen.c
+++ kernel/src/screen.c
@@ -1,12 +1,49 @@
-#include <screen.h>
+#include "screen.h"
 
-#include <tacos64/string.h>
+#include <stdarg.h>
+
+#include "snprintf.h"
+#include "tacos64/string.h"
 
 #define COLS 80
 #define ROWS 25
 
 static volatile char* const video = (volatile char*) 0xB8000;
 
+static int cursor_row = 0;
+static int cursor_col = 0;
+
+static void putch(char ch);
+
 void screen_clear(void) {
   memset((void*)video, 0, 2 * ROWS * COLS);
+}
+
+void screen_printf(const char* fmt, ...) {
+  char p[256];
+  va_list ap;
+  int n;
+
+  va_start(ap, fmt);
+  n = vsnprintf(p, 256 - 1, fmt, ap);
+  va_end(ap);
+
+  for (int i = 0; i < n; i++) {
+    putch(p[i]);
+  }
+}
+
+static void putch(char ch) {
+  video[2 * (cursor_row * COLS + cursor_col) + 0] = ch;
+  video[2 * (cursor_row * COLS + cursor_col) + 1] = 0x1f;
+
+  cursor_col++;
+  if (cursor_col == COLS) {
+    cursor_col = 0;
+    cursor_row++;
+  }
+  if (cursor_row == ROWS) {
+    // TODO: Scroll screen.
+    cursor_row = 0;
+  }
 }
