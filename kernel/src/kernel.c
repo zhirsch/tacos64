9ccf5436e0649b8bafc010e1554e500e5ab64036
Zachary Hirsch <zhirsch@umich.edu>
2024-10-24 18:41:08 -0400

    WIP

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -4,9 +4,10 @@
 #include "string/snprintf.h"
 #include "terminal/terminal.h"
 
-void kernel_main(void) {
+void kernel_main(unsigned int multiboot2_magic, unsigned int multiboot2_info) {
   clear_screen();
   kprintf("Welcome to TacOS64!\n");
+  kprintf("Multiboot2 magic=0x%x info=0x%x\n", multiboot2_magic, multiboot2_info);
 
   kprintf("Initializing the tss\n");
   tss_initialize();
